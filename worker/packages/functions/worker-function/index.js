2b9e8221e21f80d8f2759ce4ffb827a17dbf41be
Raul Glogovetan <raul@glogovetan.com>
2024-07-15 00:28:54 +0300

    Update: basic worker mongo connection

--- worker/packages/functions/worker-function/index.js
+++ worker/packages/functions/worker-function/index.js
@@ -1,42 +1,39 @@
 import { createHash } from 'crypto';
-// import { isUrl } from 'check-valid-url';
-// import { MongoClient } from 'mongodb';
+import { isUrl } from 'check-valid-url';
+import { MongoClient } from 'mongodb';
 
-// const { createHash } = require('crypto');
+const md5Hash = (string) => {
+  return createHash('md5').update(string).digest('hex');
+};
 
 export async function main(event, context) {
   // Poor man auth
   if (event?.http?.headers?.['x-auth-bearer'] !== process.env.BEARER) {
-    return { statusCode: 401, body: 'Unauthorized' };
+    return { body: 'Unauthorized' };
   }
 
+  try {
+    const { url } = event;
+    if (!url || typeof url !== 'string' || !isUrl(url)) {
+      console.error('Invalid URL');
+      return { body: 'Invalid URL' };
+    }
+    const client = new MongoClient(process.env.MONGO_CONNECTION_STRING);
+    const database = client.db('urls');
+    const collection = database.collection('default');
 
-  console.log('event', event);
-  console.log('context', context);
-  const md5Hash = (string) => {
-    return createHash('md5').update(string).digest('hex');
-  };
+    // see if url already exists
+    const urlId = md5Hash(url);
+    const [existingEntry] = await collection.find({ internalId: '123' }).toArray();
 
-  // try {
-  //   const { url } = args;
-  //   if (!url || typeof url !== 'string' || !isUrl(url)) {
-  //     console.error('Invalid URL');
-  //     return null;
-  //   }
-  //   const client = new MongoClient(process.env.MONGO_CONNECTION_STRING);
-  //   const database = client.db('urls');
-  //   const collection = database.collection('default');
+    console.log('existingEntry', existingEntry);
+    return { body: { existingEntry, urlId }}
 
-  //   // see if url already exists
-  //   const urlId = md5Hash(url);
-  //   const [existingEntry] = await collection.find({ internalId: '123' }).toArray();
-
-  //   console.log('existingEntry', existingEntry);
-
-  // } catch (error) {
-  //   console.error(error)
-  // }
-  return { body: { event, context, md5: md5Hash('worker') } };
+  } catch (error) {
+    console.error(error)
+    return { body: { error } };
+  }
+  // return { body: { event, context, md5: md5Hash('worker') } };
 }
 
 // const returnValue = await main({ "url": "https://google.com" })
